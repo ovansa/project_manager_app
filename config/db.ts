a57ddbf43afaf577f8bfe822cb5c5d6619564c34
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2022-05-23 14:32:11 +0100

    Setup test to use local db, setup eslint

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 import logger from '../src/utils/logger';
+import config from './config';
+
+const {
+  db: { db_url },
+} = config;
+
+const env = process.env.NODE_ENV as string;
+logger.info(`The DB URL for ${env} is ${db_url}`);
 
 export const connectToDB = async () => {
-  const dbUri = process.env.MONGO_URI as string;
-
   try {
-    const conn = await mongoose.connect(dbUri);
+    const conn = await mongoose.connect(db_url);
     logger.info(`DB Connected: ${conn.connection.host}`);
   } catch (err) {
     logger.error(`Could not connect to DB: ${err}`);
