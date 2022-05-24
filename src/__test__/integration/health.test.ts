7785640a7e836431c52572b26826184b98ffd108
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2022-05-24 22:57:28 +0100

    Update test

--- src/__test__/integration/health.test.ts
+++ src/__test__/integration/health.test.ts
@@ -11,7 +11,7 @@ describe('Test Health', () => {
   it('should return valid health status', async () => {
     const res = await await request(server).get('/');
 
-    expect(res.status).toBe(20);
+    expect(res.status).toBe(200);
     expect(res.body.success).toBe(true);
     expect(res.body.message).toBe('Alhamdulillah!');
   });
