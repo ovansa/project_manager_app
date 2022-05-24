7a053ba80ea87bb4814d54e30a09cf58350f2089
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2022-05-24 22:54:31 +0100

    Update test to fail

--- src/__test__/integration/health.test.ts
+++ src/__test__/integration/health.test.ts
@@ -2,18 +2,17 @@ import request from 'supertest';
 import { server } from '../../../server';
 import { disconnectDB } from '../../../config/db';
 
-afterEach(async () => {
-  return await disconnectDB();
+afterAll(async () => {
+  await disconnectDB();
+  await server.close();
 });
 
 describe('Test Health', () => {
   it('should return valid health status', async () => {
     const res = await await request(server).get('/');
 
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(20);
     expect(res.body.success).toBe(true);
     expect(res.body.message).toBe('Alhamdulillah!');
-
-    await server.close();
   });
 });
